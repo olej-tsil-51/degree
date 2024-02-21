9d8c0f4ebfaac9e8a1cf8a996b7651f9609ea18b
oleg-tsiliuric <olej.tsil@gmail.com>
2024-02-21 06:10:39 +0200

    module углы

---
+++ degree.go
@@ -0,0 +1,19 @@
+package углы
+
+import "math"
+
+func d2r(dg float64) float64 {
+	return dg / 180 * math.Pi
+}
+
+func Cos(dg float64) float64 {
+	return math.Cos(d2r(dg))
+}
+
+func Sin(dg float64) float64 {
+	return math.Sin(d2r(dg))
+}
+
+func Tan(dg float64) float64 {
+	return math.Tan(d2r(dg))
+}
